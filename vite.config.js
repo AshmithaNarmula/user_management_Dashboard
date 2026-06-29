aaf1010c91e42582979741f016fcb8c6eb768eaf
AshmithaNarmula <ashmithanarmula@gmail.com>
2026-06-29 15:23:12 +0530

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/user_management_Dashboard/", 
+  base: "/user_management_Dashboard/",
 });
